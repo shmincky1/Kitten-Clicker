d017845620e491ab6e79004b58b4911d355f6510
shmincky1 <shmincky1@gmail.com>
2016-11-22 11:50:16 -0600

    making food manually and putting out food work in a good way

--- JS/clicker.js
+++ JS/clicker.js
@@ -1,49 +1,28 @@
-// universal variables
 var foodCount = 0;
 var kittenCount = 0;
 var NEFMessage = true;
 
-// universal functions
+// universal/common functions
 function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-// onload stuff
-$('document').onLoad(function() {
-  while (true) {
-    // eventually this will keep the counters synced with the variables
-    // $('')
-  }
-});
-
-function onLoad() {
-  $('#foodCount').html(foodCount);
-  $('#kittenCount').html(kittenCount);
+// things to do when page loads
+window.onLoad = function() {
+  $('#foodCount').innerHTML = foodCount;
+  $('#kittenCount').innerHTML = kittenCount;
   $('#notEnoughFood').hide();
   NEFMessage = false;
 }
 
-// function wait(milliseconds){
-//   setTimeout(function() {
-//     console.log("Waiting has finished.");
-//   }, milliseconds);
-// }
+// button actions
+$('#makeFood').click(function(){
+    foodCount++;
+    $('#foodCount').html(foodCount)
+});
 
-// Usage!
-
-// async function demo() {
-//   console.log('Taking a break...');
-//   await sleep(2000);
-//   console.log('Two second later');
-// }
-
-function makeFood() {
-  foodCount++;
-  $('#foodCount').html(foodCount);
-}
-
-function putOutFood() {
-  if (foodCount >= 1){
+$('#putOutFood').click(function(){
+  if (foodCount >= 1) {
     foodCount--;
     kittenCount++;
     $('#foodCount').html(foodCount);
@@ -56,4 +35,4 @@ function putOutFood() {
       NEFMessage = false;
     });
   }
-}
+});
