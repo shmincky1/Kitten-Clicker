96bd9b1ce79ea62031ac9e2a3f84a5eefa17be9b
Charlie Moonen <shmincky1@gmail.com>
2016-11-25 15:38:33 -0600

    sync

--- js/clicker.js
+++ js/clicker.js
@@ -1,19 +1,34 @@
-// count variables
+// variables
 var foodCount = 0;
 var kittenCount = 0;
-var 
+var kittenFedCount = 0;
+var kittenFedDiff = kittenCount - kittenFedCount;
+var dayNumber = 1;
+var dayCountdown = 0;
 // ui variables
 var NEFMessage = true;
 
-// central loop functions
+// display loop functions
 function displayLoop() {
   $('#foodCount').html(foodCount);
   $('#kittenCount').html(kittenCount);
+  $('#dayCountdown').html(dayCountdown);
+  $('#dayNumber').html(dayNumber)
   setTimeout(displayLoop, 10);
 }
 
-function logicLoop() {
-  
+// daily functions
+function dayEvent() {
+  dayCountdown = 60;
+  function dayCountdownFunction() {
+    if (countdown > 0) {
+      dayCountdown--;
+      setTimeout(dayCountdownFunction, 1000)
+    }
+  }
+  kittenCount -= kittenFedDiff;
+  dayNumber++;
+  setTimeout(dayEvent, 1)
 }
 
 // things to do when page loads
@@ -22,15 +37,15 @@ function onLoad() {
   $('#notEnoughFood').hide();
   NEFMessage = false;
   displayLoop();
-  
+  dayEvent();
 }
 
 // button actions
-$('#makeFood').click(function(){
-    foodCount++;
+$('#makeFood').click(function() {
+  foodCount++;
 });
 
-$('#putOutFood').click(function(){
+$('#putOutFood').click(function() {
   if (foodCount >= 1) {
     foodCount--;
     kittenCount++;
